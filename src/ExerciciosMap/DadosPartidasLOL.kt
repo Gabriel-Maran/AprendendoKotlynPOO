89aba91f454c0f5437cf4d3aef7f397f87fd65b3
GabrielMaran <gabriel.enzo.libero.maran@gmail.com>
2025-05-24 20:34:09 -0300

    Dados Partidas LOL - Atividade concluida

--- src/ExerciciosMap/DadosPartidasLOL.kt
+++ src/ExerciciosMap/DadosPartidasLOL.kt
@@ -1,7 +1,26 @@
 package ExerciciosMap
 
-fun main() {
+import com.sun.management.GcInfo
 
+fun main() {
+    val player1 = Player("Gabriel");
+    player1.addDanoSofrido(10.0);
+    player1.addDanoMedio(100.0)
+    player1.addDanoTotal(1059.9)
+    player1.addNumMortes(10.0)
+    player1.addNumAbates(19.0)
+    val player2 = Player("Joao");
+    player2.addDanoSofrido(12.0);
+    player2.addDanoMedio(102.0)
+    player2.addDanoTotal(1061.9)
+    player2.addNumMortes(12.0)
+    player2.addNumAbates(21.0)
+
+    val equipe: Equipe = Equipe("EquipeFoda")
+    equipe.addAllPlayerInfo(player1)
+    equipe.addAllPlayerInfo(player2)
+
+    equipe.average()
 }
 
 class Equipe(val nome: String) {
@@ -11,19 +30,28 @@ class Equipe(val nome: String) {
         informacoesEquipe[player.nome] = player.informcoes
     }
 
-    fun media() {//TODO
-        println("A media de")
-        //- Dano Total,
-        //- Dano Médio,
-        //- Dano Sofrido Médio.
-        //- Distância Percorrida,
-        //
-        //- Número de Mortes (derrota),
-        //- Número de Abatimento (inimigos mortos).
+    fun average() {
+        val nomes: MutableSet<String> = informacoesEquipe.keys
+
+        println("A media de: ")
+        println("Dano Total: ${getAverageOfEachInfomation(nomes, "DanoTotal")}")
+        println("Dano Médio: ${getAverageOfEachInfomation(nomes, "DanoMedio")}")
+        println("Dano Sofrido Médio: ${getAverageOfEachInfomation(nomes, "danoSofrido")}")
+        println("Distância Percorrida: ${getAverageOfEachInfomation(nomes, "numMortes")}")
+        println("Dano Total: ${getAverageOfEachInfomation(nomes, "numAbates")}")
     }
+
+    fun getAverageOfEachInfomation(keys: MutableSet<String>, codToAcessPlayerInfo: String): Double {
+        var total: Double = 0.0;
+        for (key in keys) {
+            total = (informacoesEquipe.get(key)?.get(codToAcessPlayerInfo) ?: 0.0) + total
+        }
+        return total / keys.size
+    }
+
 }
 
-class Player(val nome:String) {
+class Player(val nome: String) {
     val informcoes: LinkedHashMap<String, Double> = linkedMapOf()
     fun addDanoTotal(danoTotal: Double) {
         informcoes["DanoTotal"] = danoTotal
